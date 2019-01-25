96a60c6d47d216de8a5f4186f46d743a1fc1630c
washworth01 <washworth01@gmail.com>
2019-01-25 14:57:50 +0000

    Fixed Category buttons to work without api mapping

--- filmsTable.js
+++ filmsTable.js
@@ -1,5 +1,5 @@
 let search;
-let requestURL = "http://127.0.0.1:8080/api/film"
+let requestURL = "http://192.168.1.119:8080/api/film"
 let request = new XMLHttpRequest();
 request.open("GET", requestURL);
 request.setRequestHeader('Content-type', 'application/json');
@@ -11,9 +11,11 @@ let txt = "";
 let page = 1;
 let ageCheckbox = [document.getElementById("r"), document.getElementById("nc-17"), document.getElementById("pg-13"), document.getElementById("pg"), document.getElementById("g")]
 let table = jsonString;
+let baseTable;
 
 request.onload = function() {
 	jsonString = request.response;
+	baseTable = jsonString;
 	loadTable(jsonString);
 }
 
@@ -38,14 +40,14 @@ function loadTable(newTable){
     txt = "";
 	document.getElementById("filmList").innerHTML = txt;
 	txt += "<tr><td>Title</td><td>Description</td><td>Catergory</td><td>Price</td><td>Length</td><td>Rating</td><td>Actors</td></tr>"
-    for (i = i; i < newTable.length; i++){
-        txt += "<tr><td>" + newTable[i].title + "</td>"
-        txt += "<td>" + newTable[i].description + "</td>"
-        txt += "<td>" + newTable[i].category + "</td>"
-        txt += "<td>" + newTable[i].price + "</td>"
-        txt += "<td>" + newTable[i].length + "</td>"
-        txt += "<td>" + newTable[i].rating + "</td>"
-        txt += "<td>" + newTable[i].actors + "</td></tr>"
+    for (i = i; i < table.length; i++){
+        txt += "<tr><td>" + table[i].title + "</td>"
+        txt += "<td>" + table[i].description + "</td>"
+        txt += "<td>" + table[i].category + "</td>"
+        txt += "<td>" + table[i].price + "</td>"
+        txt += "<td>" + table[i].length + "</td>"
+        txt += "<td>" + table[i].rating + "</td>"
+        txt += "<td>" + table[i].actors + "</td></tr>"
 		counter++;
 		if (counter == 20){
 			break;
@@ -63,16 +65,36 @@ function loadTable(newTable){
 // }
 
 function getByCategory(cat){
-    if (cat == "all")
-    {
-        requestURL = "http://127.0.0.1:8080/api/film";
+	let counter = 0;
+	let i = 0;
+	if (page == 1){
+		i = 0;
+	}
+	else{
+		i = page*20;
+	}
+    txt = "";
+	document.getElementById("filmList").innerHTML = txt;
+	txt += "<tr><td>Title</td><td>Description</td><td>Catergory</td><td>Price</td><td>Length</td><td>Rating</td><td>Actors</td></tr>"
+    for (i = i; i < jsonString.length; i++){
+		if(jsonString[i].category == cat){
+			txt += "<tr><td>" + jsonString[i].title + "</td>"
+			txt += "<td>" + jsonString[i].description + "</td>"
+			txt += "<td>" + jsonString[i].category + "</td>"
+			txt += "<td>" + jsonString[i].price + "</td>"
+			txt += "<td>" + jsonString[i].length + "</td>"
+			txt += "<td>" + jsonString[i].rating + "</td>"
+			txt += "<td>" + jsonString[i].actors + "</td></tr>"
+			counter++;
+			if (counter == 20){
+				break;
+			}
+		}
     }
-    else{
-        requestURL = "http://127.0.0.1:8080/api/film/category/" + cat;
-    }
-	page = 1;
-    getData();
-    loadTable(jsonString);
+    console.log(jsonString[0].category);
+  //  let jsonString = json.parse(request.stringify);
+    let myH1 = document.createElement('h1');
+    document.getElementById("filmList").innerHTML = txt;
 }
 
 function findByTitle(){
@@ -97,6 +119,7 @@ function ageFilter(){
 	let numberOfBoxesChecked = 0;
 	let checkedBoxes = [];
 	let ageTable = [];
+	let newTable = baseTable;
 	for(let i = 0; i < ageCheckbox.length; i++){
 		if(ageCheckbox[i].checked == true){
 			numberOfBoxesChecked++;
@@ -108,13 +131,18 @@ function ageFilter(){
 		counter = 0;
 		console.log(checkedBoxes.length);
 		txt += "<tr><td>Title</td><td>Description</td><td>Catergory</td><td>Price</td><td>Length</td><td>Rating</td><td>Actors</td></tr>"
-		for (i = 0; i < table.length; i++){
+		for (i = 0; i < baseTable.length; i++){
 			for(let j = 0; j < numberOfBoxesChecked; j++){
-				if(table[i].rating == checkedBoxes[j].name){
-				ageTable[counter] = table[i];
-				counter++;
+				console.log(j);
+				if(newTable[i].rating == checkedBoxes[j].name){
+				ageTable.push(newTable[i]);
 				break;
 				}
+				else if(baseTable[i].rating == checkedBoxes[j].name){
+				ageTable.push(newTable[i]);
+				break;
+				}
+				
 			}
 		}
 	console.log(table.length);		
@@ -126,17 +154,18 @@ function priceFilter(){
 	let lessThan = document.getElementById("lessThan").value;
 	let greaterThan = document.getElementById("greaterThan").value;
 	let counter = 0;
+	let newTable = baseTable;
 	txt = "";
 	priceTable = []
 	console.log(lessThan.value);
 	console.log(greaterThan.value);
 	
 	txt += "<tr><td>Title</td><td>Description</td><td>Catergory</td><td>Price</td><td>Length</td><td>Rating</td><td>Actors</td></tr>"
-	for (i = 0; i < table.length; i++){
-		if(table[i].price < lessThan && table[i].price > greaterThan){
-			priceTable[counter] = table[i];
-			counter++;				
+	for (i = 0; i < newTable.length; i++){
+		if(newTable[i].price < lessThan && newTable[i].price > greaterThan){
+			priceTable.push(newTable[i]);	
 		}
+		
 	}
 	console.log(table.length);
 	loadTable(priceTable);
@@ -155,5 +184,3 @@ function previousPage(){
 		loadTable(table);
 	}
 }
-
-
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let search;
-let requestURL = "http://127.0.0.1:8080/api/film"
+let requestURL = "http://192.168.1.119:8080/api/film"
 let request = new XMLHttpRequest();
 request.open("GET", requestURL);
 request.setRequestHeader('Content-type', 'application/json');
